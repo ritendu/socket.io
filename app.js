6799588151ef0168a522b2b32c2f3f8ffe408549
Ritendu Sen <ritendu.sen@weavers-web.com>
2022-09-25 23:43:42 +0530

    wrap it with object-message.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path');
+const formatMessage = require('./utils/message');
 app.use(express.static(path.join(__dirname,'public')));
 const server = require('http').createServer(app);
 
@@ -17,14 +18,14 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json());
 io.on('connection',(socket)=>{
   console.log('New Connection'+socket.id);
-  socket.emit('message','Welcome to Chatcord!');
-  socket.broadcast.emit('message','A user has joined the chat');
+  socket.emit('message',formatMessage('ChatCord Bot','Welcome to Chatcord!'));
+  socket.broadcast.emit('message',formatMessage('ChatCord Bot','A user has joined the chat'));
   socket.on('disconnect',()=>{
-    io.emit('message','A user has left the chat')
+    io.emit('message',formatMessage('ChatCord Bot','A user has left the chat'))
   })
   socket.on('chatMessage',(msg)=>{
 // console.log(msg);
-io.emit('message',msg)
+io.emit('message',formatMessage('username',msg))
   })
 })
 server.listen(PORT, () => {
--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,31 @@
 const chatForm = document.getElementById('chat-form');
+//Scroll
+const chatMessages = document.querySelector('.chat-messages');
 const socket = io();
+//Show message
 socket.on('message',message=>{
     console.log(message);
-outputMessage(message);
+//Show message-design
+    outputMessage(message);
+//Scroll
+chatMessages.scrollTop = chatMessages.scrollHeight;
 })
 
 chatForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const msg = e.target.elements.msg.value;
-    socket.emit('chatMessage',msg)
+    socket.emit('chatMessage',msg);
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+   
 })
 
 function outputMessage(message){
 const div = document.createElement('div');
 div.classList.add('message');
-div.innerHTML = `<p class="meta">Brad<span>9:12pm</span></p>
+div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
 <p class="text">
-${message}
+${message.text}
 </p>`;
 document.querySelector('.chat-messages').appendChild(div)
 }
